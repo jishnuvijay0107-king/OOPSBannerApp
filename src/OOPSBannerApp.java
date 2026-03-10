2eda55de99ee1f6abc8a0eeef900ba474e2ecef0
jishnuvijay0107-king <jishnuvijay0107@gmail.com>
2026-03-10 09:38:17 +0530

    added src uc3

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,28 @@
+/**
+ * OOPSBannerApp
+ * Displays the OOPS banner using a method.
+ *
+ * @author Jishnu
+ * @version 1.2
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        printBanner();
+
+    }
+
+    public static void printBanner() {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println(" *****    *****    *        ***** ");
+
+    }
+}
