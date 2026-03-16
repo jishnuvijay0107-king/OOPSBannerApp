698f45a920b395aaa73faabfb0c2af6eb558cd8e
jishnuvijay0107-king <jishnuvijay0107@gmail.com>
2026-03-16 11:11:17 +0530

    implemented UC4

---
+++ src/OOPSBanner.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * Displays the OOPS banner using a method.
+ *
+ * @author Jishnu
+ * @version 1.2
+ */
+
+public class OOPSBanner {
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
+    }
+}
